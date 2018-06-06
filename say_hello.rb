56e10fcd0ef49246a3b471c0c9664af1eaeb29e2
lilycampbell <lilycatcampbell@gmail.com>
2018-06-06 21:34:10 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
-# def say_hello (your_name = "Ruby Programmer")
-#   puts "Hello #{your_name}!"
-# end
+def say_hello (your_name = "Ruby Programmer")
+  puts "Hello #{your_name}!"
+end
 
-# say_hello
+say_hello
 
 
